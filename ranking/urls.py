e2c0a39e9e2928d7ffc42e3a30a5d719c2facb47
jdheeee <cloudymmer@yonsei.ac.kr>
2026-07-20 01:57:18 +0900

    feat: 랭킹 시스템 구현

--- ranking/urls.py
+++ ranking/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+ 
 urlpatterns = [
     path('', views.ranking_view, name='ranking'),
+    path('data/', views.ranking_data, name='ranking_data'),
 ]
