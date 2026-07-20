7fec3225dc8d4c08a89b078bed8b2e06c840c13d
jdheeee <cloudymmer@yonsei.ac.kr>
2026-07-20 19:45:02 +0900

    style: 폰트 및 랭킹 스크롤 수정

--- ranking/views.py
+++ ranking/views.py
@@ -5,10 +5,13 @@ from django.http import JsonResponse
 User = get_user_model()
 
 
-def _build_ranked_list(limit=5):
+def _build_ranked_list(limit=None):
     """점수 내림차순 정렬 후, 동점자는 같은 순위를 부여하고
     다음 순위는 그만큼 건너뛰는 방식(1, 2, 2, 4 ...)으로 순위 리스트를 만든다."""
-    users = list(User.objects.all().order_by('-score')[:limit])
+    queryset = User.objects.all().order_by('-score')
+    if limit is not None:
+        queryset = queryset[:limit]
+    users = list(queryset)
 
     ranked = []
     rank = 0
@@ -23,13 +26,13 @@ def _build_ranked_list(limit=5):
 
 
 def ranking_view(request):
-    ranked_users = _build_ranked_list(limit=5)
+    ranked_users = _build_ranked_list()  # 전체 유저 표시
     return render(request, 'ranking.html', {'ranked_users': ranked_users})
 
 
 def ranking_data(request):
     """실시간 갱신용 JSON API. 프론트에서 주기적으로 fetch해서 화면을 다시 그림."""
-    ranked_users = _build_ranked_list(limit=5)
+    ranked_users = _build_ranked_list()  # 전체 유저 표시
     data = [
         {
             'rank': entry['rank'],
